00587ae4f29d7110a953e9e6dc9fa200d86cd403
Charles Rev <charleswrevuelta@gmail.com>
2023-08-09 02:46:55 +0800

    Added necessary clickable links for the website

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import BrandWhite from "../assets/Brand_White.png";
 import Logo_2 from "../assets/Logo2.png";
 import Appstore from "../assets/download-1.png";
@@ -10,76 +10,136 @@ import Socials_3 from "../assets/socials_3.png";
 import Socials_4 from "../assets/socials_4.png";
 import Socials_5 from "../assets/socials_5.png";
 
-
 function FooterSection() {
+  let url = "#";
   return (
-    <div className='footer-section'>
-        <div className='footer-flex-container'>
-            <div className='footer-column-1'>
-                <img src={Logo_2} id="footer-logo" alt="logo-white" />
-                <img src={BrandWhite} id="footer-brand-name" alt="brand-white" />
-                <span id='get-app'>Get the app:</span>
-                <div className='app-links-container'>
-                    <img src={Appstore} alt="Appstore-link" />
-                    <img src={Playstore} alt="Playstore-link" />
-                    <img src={Appgallery} alt="Appgallery-link" />
-                </div>
-                <span id="follow">Follow us:</span>
-                    <div className='follow-us-container'>
-                        <img src={Socials_1} alt="socials-1" />
-                        <img src={Socials_2} alt="socials-2" />
-                        <img src={Socials_3} alt="socials-3" />
-                        <img src={Socials_4} alt="socials-4" />
-                        <img src={Socials_5} alt="socials-5" />
-                    </div>
-            </div>
-            <div className='footer-column-2'>
-                <h5>About</h5>
-                <ul>
-                    <li>Company</li>
-                    <li>Leadership</li>
-                    <li>Jobs - HIRING!</li>
-                    <li>Pricing</li>
-                    <li>Press</li>
-                    <li>Investors</li>
-                </ul>
-            </div>
-            <div className='footer-column-3'>
-                <h5>Solutions</h5>
-                <ul>
-                    <li>At School</li>
-                    <li>At Work</li>
-                    <li>At Home</li>
-                </ul>
-            </div>
-            <div className='footer-column-4'>
-                <h5>Resources</h5>
-                <ul>
-                    <li>Study with Kurtis</li>
-                    <li>Blog</li>
-                    <li>Kurtis Certified</li>
-                    <li>Help Center</li>
-                    <li>Library</li>
-                    <li>Shop</li>
-                    <li>Safety center</li>
-                </ul>
-            </div>
-            <div className='footer-column-5'>
-                <h5>Terms and conditions</h5>
-                <ul>
-                    <li>Terms and Conditions</li>
-                    <li>Privacy Policy</li>
-                    <li>US Privacy Laws</li>
-                    <li>Children's Privary Policy</li>
-                    <li>Inclusion and Accessibility policy</li>
-                </ul>
-            </div>
+    <div className="footer-section">
+      <div className="footer-flex-container">
+        <div className="footer-column-1">
+          <a href={url}>
+            <img src={Logo_2} id="footer-logo" alt="logo-white" />
+            <img src={BrandWhite} id="footer-brand-name" alt="brand-white" />
+          </a>
+          <span id="get-app">Get the app:</span>
+          <div className="app-links-container">
+            <a href={url}>
+              <img src={Appstore} alt="Appstore-link" />
+            </a>
+            <a href={url}>
+              <img src={Playstore} alt="Playstore-link" />
+            </a>
+            <a href={url}>
+              <img src={Appgallery} alt="Appgallery-link" />
+            </a>
+          </div>
+          <span id="follow">Follow us:</span>
+          <div className="follow-us-container">
+            <a href={url}>
+              <img src={Socials_1} alt="socials-1" />
+            </a>
+            <a href={url}>
+              <img src={Socials_2} alt="socials-2" />
+            </a>
+            <a href={url}>
+              <img src={Socials_3} alt="socials-3" />
+            </a>
+            <a href={url}>
+              <img src={Socials_4} alt="socials-4" />
+            </a>
+            <a href={url}>
+              <img src={Socials_5} alt="socials-5" />
+            </a>
+          </div>
         </div>
-        <div className='copyright-div'>
-            <p id="copyright">Copyright @ 2020, Kurtis All rights reserved.</p>
+        <div className="footer-column-2">
+          <h5>About</h5>
+          <ul>
+            <a href={url}>
+              <li>Company</li>
+            </a>
+            <a href={url}>
+              <li>Leadership</li>
+            </a>
+            <a href={url}>
+              <li>Jobs - HIRING!</li>
+            </a>
+            <a href={url}>
+              <li>Pricing</li>
+            </a>
+            <a href={url}>
+              <li>Press</li>
+            </a>
+            <a href={url}>
+              <li>Investors</li>
+            </a>
+          </ul>
         </div>
+        <div className="footer-column-3">
+          <h5>Solutions</h5>
+          <ul>
+            <a href={url}>
+              <li>At School</li>
+            </a>
+            <a href={url}>
+              <li>At Work</li>
+            </a>
+            <a href={url}>
+              <li>At Home</li>
+            </a>
+          </ul>
+        </div>
+        <div className="footer-column-4">
+          <h5>Resources</h5>
+          <ul>
+            <a href={url}>
+              <li>Study with Kurtis</li>
+            </a>
+            <a href={url}>
+              <li>Blog</li>
+            </a>
+            <a href={url}>
+              <li>Kurtis Certified</li>
+            </a>
+            <a href={url}>
+              <li>Help Center</li>
+            </a>
+            <a href={url}>
+              <li>Library</li>
+            </a>
+            <a href={url}>
+              <li>Shop</li>
+            </a>
+            <a href={url}>
+              <li>Safety center</li>
+            </a>
+          </ul>
+        </div>
+        <div className="footer-column-5">
+          <h5>Terms and conditions</h5>
+          <ul>
+            <a href={url}>
+              <li>Terms and Conditions</li>
+            </a>
+            <a href={url}>
+              <li>Privacy Policy</li>
+            </a>
+            <a href={url}>
+              <li>US Privacy Laws</li>
+            </a>
+            <a href={url}>
+              <li>Children's Privary Policy</li>
+            </a>
+            <a href={url}>
+              <li>Inclusion and Accessibility policy</li>
+            </a>
+          </ul>
+        </div>
+      </div>
+      <div className="copyright-div">
+        <p id="copyright">Copyright @ 2020, Kurtis All rights reserved.</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default FooterSection
+export default FooterSection;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,26 @@
-
 import React from "react";
-import Logo from "../assets/Logo1.png"
+import Logo from "../assets/Logo1.png";
 
 function NavBar() {
+  let url = "#";
   return (
     <nav className="nav-bar">
-      <div className="left-navbar">
-        <img className="logo" src={Logo} alt="Logo" />
-        <h1 className="title">Kurtis Classrom</h1>
-      </div>
+      <a href={url}>
+        <div className="left-navbar">
+          <img className="logo" src={Logo} alt="Logo" />
+          <h1 className="title">Kurtis Classrom</h1>
+        </div>
+      </a>
       <div className="right-navbar">
-        <button id="signup-button-1" className="signup-button btn btn-primary">Sign up</button>
-        <button id="login-button" className="btn btn-outline-secondary">Log in</button>
+        <button id="signup-button-1" className="signup-button btn btn-primary">
+          Sign up
+        </button>
+        <button id="login-button" className="btn btn-outline-secondary">
+          Log in
+        </button>
       </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
