880d24a354a2fa7dcc50acfb79943bb24612b4af
Charles Rev <charleswrevuelta@gmail.com>
2023-08-10 01:05:56 +0800

    Added animations from Framer Motion

--- src/components/ServiceSection.jsx
+++ src/components/ServiceSection.jsx
@@ -2,20 +2,30 @@ import React from "react";
 import Doodle_2 from "../assets/Miroodles Color Composition_2.png";
 import Doodle_3 from "../assets/Miroodles Color Composition_3.png";
 import Doodle_4 from "../assets/Miroodles Color Composition_4.png";
+import { motion } from "framer-motion";
 
 function ServiceSection() {
   let url = "#";
   return (
     <div className="service-section">
       <p>
-        Still more?{" "}
+        Still more?
         <a href={url}>
           <span id="link-games">Take a look to our features games</span>
         </a>
       </p>
       <h3>How does Kurtis work?</h3>
       <div className="line-div"></div>
-      <div className="create-container">
+      <motion.div
+        className="create-container"
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1 },
+        }}
+        transition={{ duration: 2 }}
+        initial="hidden"
+        whileInView="visible"
+      >
         <img src={Doodle_2} className="doodle" alt="doodle-2" />
         <div className="service-text">
           <h3>Create</h3>
@@ -24,8 +34,17 @@ function ServiceSection() {
             any topic, in any language.
           </p>
         </div>
-      </div>
-      <div className="host-container">
+      </motion.div>
+      <motion.div
+        className="host-container"
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1 },
+        }}
+        transition={{ duration: 2 }}
+        initial="hidden"
+        whileInView="visible"
+      >
         <div id="service-text-2" className="service-text">
           <h3>Host or share</h3>
           <p>
@@ -39,8 +58,17 @@ function ServiceSection() {
           id="doodle-host"
           alt="doodle-3"
         />
-      </div>
-      <div className="play-container">
+      </motion.div>
+      <motion.div
+        className="play-container"
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1 },
+        }}
+        transition={{ duration: 2 }}
+        initial="hidden"
+        whileInView="visible"
+      >
         <img src={Doodle_4} className="doodle" alt="doodle-4" />
         <div className="service-text">
           <h3>Play</h3>
@@ -49,7 +77,7 @@ function ServiceSection() {
             questions on your device.
           </p>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
--- src/components/SignupSection.jsx
+++ src/components/SignupSection.jsx
@@ -1,24 +1,83 @@
-import React from 'react'
+import React from "react";
 import Doodle_5 from "../assets/Miroodles Color Composition_5.png";
+import { motion } from "framer-motion";
 
 function SignupSection() {
   return (
-    <div className='signup-section'>
-        <div className='highlights'>
-            <p className='highlights-1'>More than 50% of US teachers use Kurtis</p>
-            <p className='highlights-2'>Players in more than 200 countries</p>
-            <p className='highlights-3'>Over 30 million public games available</p>
-            <p className='highlights-4'>97% of the Fortune 500 use Kurtis</p>
+    <div className="signup-section">
+      <div className="highlights">
+        <motion.p
+          className="highlights-1"
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+          }}
+          transition={{ duration: 2 }}
+          initial="hidden"
+          whileInView="visible"
+        >
+          More than 50% of US teachers use Kurtis
+        </motion.p>
+        <motion.p
+          className="highlights-2"
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+          }}
+          transition={{ duration: 2 }}
+          initial="hidden"
+          whileInView="visible"
+        >
+          Players in more than 200 countries
+        </motion.p>
+        <motion.p
+          className="highlights-3"
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+          }}
+          transition={{ duration: 2 }}
+          initial="hidden"
+          whileInView="visible"
+        >
+          Over 30 million public games available
+        </motion.p>
+        <motion.p
+          className="highlights-4"
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+          }}
+          transition={{ duration: 2 }}
+          initial="hidden"
+          whileInView="visible"
+        >
+          97% of the Fortune 500 use Kurtis
+        </motion.p>
+      </div>
+      <motion.div
+        className="flex-container"
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1 },
+        }}
+        transition={{ duration: 2 }}
+        initial="hidden"
+        whileInView="visible"
+      >
+        <img src={Doodle_5} className="doodle-signup" alt="doodle-5" />
+        <div className="signup-text">
+          <h2 id="ready-text">Ready for make learning fun!</h2>
+          <button
+            id="signup-button-2"
+            className="signup-button btn btn-primary"
+          >
+            Sign up for free
+          </button>
         </div>
-        <div className='flex-container'>
-            <img src={Doodle_5} className='doodle-signup' alt="doodle-5" />
-            <div className='signup-text'>
-              <h2 id="ready-text">Ready for make learning fun!</h2>
-              <button id="signup-button-2" className="signup-button btn btn-primary">Sign up for free</button>
-            </div>
-        </div>
+      </motion.div>
     </div>
-  )
+  );
 }
 
-export default SignupSection
+export default SignupSection;
